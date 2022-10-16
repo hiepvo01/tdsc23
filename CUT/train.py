d073b237037eff478728a8ee1ff970f575ed4285
hiepvo01 <vohi0311@gmail.com>
2022-10-16 23:54:56 +1100

    cut

--- CUT/train.py
+++ CUT/train.py
@@ -9,8 +9,6 @@ from util.visualizer import Visualizer
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    print(type(dataset))
-    print(dataset.shape)
     dataset_size = len(dataset)    # get the number of images in the dataset.
 
     model = create_model(opt)      # create a model given opt.model and other options
